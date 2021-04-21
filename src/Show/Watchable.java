5cf78d2b3378851fd5d862eb9c5565eb2dd5fc50
midori-loee <chloelaw75@hotmail.com>
2021-04-20 20:51:26 -0400

    Initial commit

---
+++ src/Show/Watchable.java
@@ -0,0 +1,14 @@
+// -----------------------------------------------------
+// Part: 2A
+// Written by: Chloe Hei Yu Law - 40173275
+// -----------------------------------------------------
+
+package Show;
+
+/**
+ * Create an interface named Watchable which has a method string isOnSameTime (Show S) where S is an
+ * object of type TVShow described in the next part.
+ */
+public interface Watchable {
+    public String isOnSameTime(TVShow s);
+}
